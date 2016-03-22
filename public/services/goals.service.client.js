42e0d430f28ea01f0aee48218f998a134b141e5b
Rachana <deshmukh.r@husky.neu.edu>
2016-03-22 12:50:05 -0400

    Added goal service

---
+++ public/services/goals.service.client.js
@@ -0,0 +1,39 @@
+/**
+ * Created by rachanadeshmukh on 3/22/16.
+ */
+"use strict";
+(function(){
+    angular
+        .module("iSaveApp")
+        .factory("GoalService", GoalService);
+
+    function GoalService(){
+        var goals = [
+            { "_id":1, "name":"Car", "descr":"Purchase new car", "amount":5000,
+                "date":new Date(), "completed":false},
+            { "_id":2, "name":"Paris", "descr":"Travel to Paris", "amount":3000,
+                "date":new Date(), "completed":true},
+            { "_id":3, "name":"Coldplay", "descr":"Go to coldplay concert", "amount":200,
+                "date":new Date(), "completed":false}
+        ];
+        var api = {
+            goals: goals,
+            //getCompletedGoals: getCompletedGoals,
+           //getCurrentGoals: getCurrentGoals,
+            createGoal:  createGoal,
+            //deleteGoal: deleteGoal,
+            //updategoal: updategoal
+        };
+        return api;
+
+
+        function createGoal(goal) {
+            goal["_id"] = (new Date()).getTime();
+            goal["completed"] = false;
+            api.goals.push(goal);
+            //callback(goal);
+        }
+
+
+    }
+})();
